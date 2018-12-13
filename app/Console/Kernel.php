1faaf7d85e35dba7a6aafc6f9ea26a8864a70fd0
Joe <qsx@meitu.com>
2018-12-13 18:48:12 +0800

    补充定时任务

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\MessagesRssSend;
+use App\Console\Commands\SpiderRss;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        MessagesRssSend::class,
+        SpiderRss::class
     ];
 
     /**
